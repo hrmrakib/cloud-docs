8b568dfbd1fa5377cdfcc08925d653699adce4f8
hrmrakib <hrmrakibs@gmail.com>
2024-10-30 22:09:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className='flex items-center justify-center h-screen w-full'>
+      <h1 className='text-rose-500 font-bold text-3xl'>
+        Hello, Bangladesh 2.0
+      </h1>
+    </div>
+  );
+};
+
+export default App;
