d23efef44b682e6240f7410cd4021fd8986e6c43
hrmrakib <hrmrakibs@gmail.com>
2024-10-31 06:39:03 +0600

    add CV and Resume on a link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 const App = () => {
   return (
     <div className='flex items-center justify-center h-screen w-full'>
-      <h1 className='text-rose-500 font-bold text-3xl'>
-        Hello, Bangladesh 2.0
-      </h1>
+      <div>
+        <h1 className='text-rose-500 font-bold text-3xl'>
+          Hello, Bangladesh 2.0
+        </h1>
+        <div className='flex items-center justify-center gap-3 mt-3'>
+          <a
+            href='/CV.pdf'
+            target='_blank'
+            className='border bg-blue-500 text-white rounded px-3 py-2'
+          >
+            CV
+          </a>
+          <a
+            href='/R.pdf'
+            target='_blank'
+            className='border bg-blue-500 text-white rounded px-3 py-2'
+          >
+            Resume
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
